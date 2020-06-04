616aef5922834d853c76fa32538cff610f0ad364
Lucas <budaglucas@gmail.com>
2020-06-04 20:06:52 -0300

    shop page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss'
+//import SECTIONS_DATA from "./sections.data.js";
 
 class Directory extends React.Component {
 	constructor() {
