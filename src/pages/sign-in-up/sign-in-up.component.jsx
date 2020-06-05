99dc7312a346c249159d29bd8e3aeb17ea810dad
Lucas <budaglucas@gmail.com>
2020-06-04 22:29:58 -0300

    sign in, custom button

---
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+	<div className='sign-in-up'>
+	  <SignIn />
+	</div>
+
+)
+
+export default SignInAndSignUpPage;
