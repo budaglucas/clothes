6eda226354ab020ee1abe38f12df48b3e38cadd0
Lucas <budaglucas@gmail.com>
2020-06-06 23:27:00 -0300

    redux

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -64,7 +64,10 @@ render () {
 			 
 			 <div className='buttons'>
 				 <CustomButton type='submit'>Sign in</CustomButton>
-				 <CustomButton onClick={signInWithGoogle} isGoogleSignIn >Sign in with google</CustomButton>
+				 <CustomButton
+				 type='button' 
+				 onClick={signInWithGoogle} 
+				 isGoogleSignIn >Sign in with google</CustomButton>
 		 	</div>	
 		 </form>
 	 </div>
