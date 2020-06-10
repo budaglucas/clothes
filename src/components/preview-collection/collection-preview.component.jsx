ede844e233ea5ae3b45a80b6f61b437088e59fdc
Lucas <budaglucas@gmail.com>
2020-06-10 19:06:23 -0300

    um monte de coisa

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -11,8 +11,7 @@ const CollectionPreview = ({ title, items }) => (
 	 		.filter((item, idx) => idx < 4)
 	 		.map((item) => (
 	 	 	  <CollectionItem key={item.id} item={item} />
-	 	 	))
-	 	}
+	 	 	))}
 	 </div>	
 	</div>
 )
